5db22ae1317ec33c3b415726944629232c341fa7
karthik <karthi.m34999@gmail.com>
2023-12-06 16:58:13 +0530

    ni

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <html>
+      <body>
+        <h1>hi hero</h1>
+        <p>go here</p>
+      </body>
+    </html>
+  );
+}
+
+export default App;
